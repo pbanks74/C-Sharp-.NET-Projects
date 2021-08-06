84be6d25b2c8b3f42634736791cf08ee9cd61df9
Paul Fairbanks <paulbanksinc16@gmail.com>
2021-08-05 20:06:21 -0700

    Add comment

--- PackageExpress/PackageExpress/Program.cs
+++ PackageExpress/PackageExpress/Program.cs
@@ -17,13 +17,13 @@ namespace PackageExpress
             Console.WriteLine("\nPlease enter the package weight:");
             int pkgWeight = Convert.ToInt32(Console.ReadLine());
 
-            // gives user error message if weight is greater than 50
+            // Gives user error message if weight is greater than 50
             if (pkgWeight > 50)
             {
                 Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
                 Console.ReadLine();
             }
-            // saves user input for width, height and length
+            // Saves user input for width, height and length
             Console.WriteLine("\nPlease enter the package width:");
             int pkgWidth = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("\nPlease enter the package height:");
@@ -31,7 +31,7 @@ namespace PackageExpress
             Console.WriteLine("\nPlease enter the package length:");
             int pkgLength = Convert.ToInt32(Console.ReadLine());
 
-            // if dimensions total greater than 50, the error message is displayed
+            // If dimensions total greater than 50, the error message is displayed
             if (pkgWidth + pkgHeight + pkgLength > 50)
             {
                 Console.WriteLine("Package too big to be shipped via Package Express. Have a good day.");
