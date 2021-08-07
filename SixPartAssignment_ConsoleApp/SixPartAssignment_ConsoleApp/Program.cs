bf37a49094a0624b9b427fb217dbb1e1d71d1f36
Paul Fairbanks <paulbanksinc16@gmail.com>
2021-08-07 14:17:15 -0700

    Create App

---
+++ SixPartAssignment_ConsoleApp/SixPartAssignment_ConsoleApp/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        //============== PART 1 =========================================================
+
+        // Greeting
+        Console.WriteLine("Six Part C# Tech Academy Assignment");
+        Console.WriteLine("\nBy: Paul Fairbanks, 2021");
+        Console.WriteLine("\n=======================================");
+
+
+        // creates a  one-dimensional array of strings
+        string[] namesArray = { "The kayak is ", "The boat is ", "The car is ", "The house is ", "The dog is "};
+
+        // asks the user to input text and saves it as a variable
+        Console.WriteLine("Please enter your favorite color:");
+        string userColor = Console.ReadLine();
+
+        // a loop that iterates through each string and adds user's input end of each
+        for (int p = 0; p < namesArray.Length; p++)
+        {
+            string.Concat(userColor);
+        }
+
+        // a loop that prints off each string in the array on a seperate line
+        
+        // prints namesArray[1] to test if for loop works
+        Console.WriteLine(namesArray[1]);
+
+
+        Console.ReadLine();
+        }
+    }
+
