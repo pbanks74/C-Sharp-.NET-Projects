c2af66cf8dcc0ae3aaac149fe7f4be4c4fb0360a
Paul Fairbanks <paulbanksinc16@gmail.com>
2021-08-12 19:07:16 -0700

    Update Program.cs

--- SixPartAssignment_ConsoleApp/SixPartAssignment_ConsoleApp/Program.cs
+++ SixPartAssignment_ConsoleApp/SixPartAssignment_ConsoleApp/Program.cs
@@ -34,10 +34,92 @@ using System.Collections.Generic;
         {
             Console.WriteLine(p);
         }
-        
+
         // prints namesArray[1] to test if for loop works
         //Console.WriteLine(namesArray[1]);
 
+
+
+        //============== PART 2 =========================================================
+
+        // created indefinate loop 
+        int z = 0;
+        while (true)
+        {
+            Console.WriteLine("z = {0}", z);
+
+            z++;
+
+            // created break to stop the loop at i = 10
+            if (z > 10)
+                break;
+        }
+
+
+
+        //============== PART 3 =========================================================
+
+        // created loop that uses "<"
+        int i = 0;
+        while (true)
+        {
+            Console.WriteLine("i = {0}", i);
+
+            i++;
+
+            // created break to stop the loop at i = 10
+            if (i > 10)
+                break;
+        }
+
+        // created loop that uses "<="
+        int x = 1;
+
+        // Exit when x becomes greater than 6
+        while (x <= 6)
+        {
+            Console.WriteLine("This is my loop using less than or equal to operator");
+            
+                x++;
+        }
+
+
+
+        //============== PART 4 =========================================================
+        
+        List<string> ColorList = new List<string>();
+        ColorList.Add("green");
+        ColorList.Add("blue");
+        ColorList.Add("yellow");
+        ColorList.Add("orange");
+        ColorList.Add("brown");
+        ColorList.Add("black");
+        ColorList.Add("red");
+        ColorList.Add("pink");
+        ColorList.Add("white");
+
+        Console.WriteLine("\nPlease enter a color to see if there is a match on the list:");
+        string input = Console.ReadLine();
+
+        for (int z = 0; z < ColorList.Count; z++)
+        {
+            if (input == color)
+            {
+                Console.WriteLine("Yes " + color + " is on the list!");
+            }
+            else
+            {
+                Console.WriteLine("Sorry " + input + " is not on the list.");
+            }
+        }
+
+
+
+        //============== PART 5 =========================================================
+
+        
+
+        //============== PART 6 =========================================================
 
 
         Console.ReadLine();
