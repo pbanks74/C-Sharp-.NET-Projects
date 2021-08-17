af723b922bf5e74d7731a211a81e1ecceb5d1d92
Paul Fairbanks <paulbanksinc16@gmail.com>
2021-08-17 14:55:18 -0700

    create class with function

---
+++ MethodsConsoleApp/MethodsConsoleApp/Math.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsConsoleApp
+{
+    // created new class and made public
+    public class Math
+    {
+        public Math()
+        {
+
+        }
+        // method to add
+        public int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        // method to multiply
+        public int Multiply(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+        // method to subtract
+        public int Subtract(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+    }
+}
---
+++ MethodsConsoleApp/MethodsConsoleApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // asks the user to enter a number and assigns input to a variable
+            Console.WriteLine("Please enter a number to perform math operations on:");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            // instantiates the class and assigns the instance to a variable
+            Math math = new Math();
+            int result = 0;
+            int result2 = 0;
+            int result3 = 0;
+
+            // creates a variable for each math operation
+            result = math.Add(userInput,10);
+            result2 = math.Multiply(userInput, 10);
+            result3 = math.Subtract(userInput, 10);
+
+            // prints results of the calculations to screen
+            Console.WriteLine(userInput + " plus ten equals " + result);
+            Console.WriteLine(userInput + " times ten equals " + result2);
+            Console.WriteLine(userInput + " minus ten equals " + result3);
+
+            Console.ReadLine();
+        }
+        
+    }
+}
