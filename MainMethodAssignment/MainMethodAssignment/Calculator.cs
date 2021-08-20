d504d8454ccfa78377027682223e7d26e0d6a734
Paul Fairbanks <paulbanksinc16@gmail.com>
2021-08-20 12:20:48 -0700

    create console app

---
+++ MainMethodAssignment/MainMethodAssignment/Calculator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    public class Calculator
+    {
+        public Calculator()
+        {
+        }
+        // method to add integers
+        public int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        // method to add decimals
+        public decimal Add(decimal d1, decimal d2)
+        {
+            return Decimal.ToInt32(d1 + d2);
+        }
+        
+    }
+}
---
+++ MainMethodAssignment/MainMethodAssignment/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // asks the user to enter a number and assigns input to a variable
+            Console.WriteLine("Please enter a number to perform math operations on:");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            // instantiates the class and assigns the instance to a variable
+            Calculator calculator = new Calculator();
+            int result1 = 0;
+            
+            // creates a variable for the math operation
+            result1 = calculator.Add(userInput, 10);
+            
+            // displays anser to the screen
+            Console.WriteLine(userInput + " plus ten equals: " + result1);
+
+            Console.WriteLine("Please enter a decimal to perform math operations on:");
+            decimal userInput2 = Convert.ToDecimal(Console.ReadLine());
+            decimal result2 = 0;
+            result2 = (int)calculator.Add(userInput2, .3m);
+            Console.WriteLine(userInput2 + " plus three tenths equals: " + result2);
+
+            Console.ReadLine();
+        }
+    }
+}
