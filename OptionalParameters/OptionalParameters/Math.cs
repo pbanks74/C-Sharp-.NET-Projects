3d4d74e8f6b751da6eabf052c8b008dab83ccbc9
Paul Fairbanks <paulbanksinc16@gmail.com>
2021-08-21 11:58:38 -0700

    create method

---
+++ OptionalParameters/OptionalParameters/Math.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionalParameters
+{
+    public class Math
+    {
+        public Math()
+        {
+
+        }
+        // method that takes in optional parameters
+        public int Add(int num1, int num2 = 10)
+        {
+            return num1 + num2;
+        }
+    }
+}
---
+++ OptionalParameters/OptionalParameters/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OptionalParameters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // instantiates the class
+            Math math = new Math();
+
+            // asks the user to input numbers and saves as variables
+            Console.WriteLine("Please enter a number:");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Optional, enter a second number:");
+            int userInput2 = Convert.ToInt32(Console.ReadLine());
+
+            // calls the method and passes in the variables
+            int result1 = math.Add(userInput, userInput2);
+
+            // displays the results 
+            Console.WriteLine(result1);
+
+            Console.ReadLine();
+        }
+    }
+}
