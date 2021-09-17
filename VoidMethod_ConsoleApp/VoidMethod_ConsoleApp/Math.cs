dac2bfacf1bf7c8cd472846df371c60f6ee7f4ce
Paul Fairbanks <paulbanksinc16@gmail.com>
2021-09-17 13:23:04 -0700

    Create Console

---
+++ VoidMethod_ConsoleApp/VoidMethod_ConsoleApp/Math.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethod_ConsoleApp
+{
+    public class Math
+    {
+        public Math()
+        {
+          
+        }
+        // Void Method that takes two integers as parameters.
+        // Does a math operation on the first integer 
+        public int Add(int num1, int num2)
+        {
+            return num1 + 1;
+        }
+    }
+}
---
+++ VoidMethod_ConsoleApp/VoidMethod_ConsoleApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VoidMethod_ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // instantiates the class
+            Math math = new Math();
+
+            // asks the user to input a number and saves as a variable
+            Console.WriteLine("Please enter a number:");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            //asks the user to enter a second number and saves as a variable
+            Console.WriteLine("Please enter a second number:");
+            int userInput2 = Convert.ToInt32(Console.ReadLine());
+
+            // calls the method and passes in the variables
+            int result1 = math.Add(userInput, userInput2);
+
+            // displays the results of the math operation
+            Console.WriteLine("The first number plus one equals " + result1);
+            // displays the second integer to the screen
+            Console.WriteLine("The second number is " + userInput2);
+
+            Console.ReadLine();
+
+        }
+    }
+}
