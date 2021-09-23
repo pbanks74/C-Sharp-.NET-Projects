6bb76386209409d8cdfb7a182ad01c3d7d9be32a
Paul Fairbanks <paulbanksinc16@gmail.com>
2021-09-22 20:33:59 -0700

    create console

---
+++ ConsoleAppDivide/ConsoleAppDivide/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppDivide
+{
+    // declared class as static *prevents the class from being instantiated*
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            // instantiates the class
+            divide divide = new divide();
+
+            // asks the user to input a number and saves as a variable
+            Console.WriteLine("Please enter a number:");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            // calls the method and passes in the variable
+            int result1 = divide.Divide(userInput);
+
+            // displays the results of the math operation
+            Console.WriteLine(userInput + " divided by two equals " + result1);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleAppDivide/ConsoleAppDivide/divide.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppDivide
+{
+    
+    public class divide
+    {
+        public divide()
+        {
+
+        }
+        // Void method that outputs an integer
+        // divides the data passed to it by 2
+        public int Divide(int a1, int a2 = 2)
+        {
+            return a1 / a2;
+        }
+        // method with output parameters
+        public int Divide(int b1, out int outParameter, int b2 = 4)
+        {
+            outParameter = 10;
+            return b1 / b2;
+        }
+        
+    }
+}
