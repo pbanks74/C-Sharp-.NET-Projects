763607dc000e8705be0d629ac34e8ef536acc0de
Paul Fairbanks <paulbanksinc16@gmail.com>
2021-09-25 16:04:02 -0700

    create console

---
+++ OverloadConsoleApp/OverloadConsoleApp/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadConsoleApp
+{
+    // created Employee class and assigned it a property
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        public static Employee operator== (Employee employee)
+        {
+            employee.Id.Equals(employee.Id);
+            return employee;
+        }
+    }
+}
---
+++ OverloadConsoleApp/OverloadConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+        }
+    }
+}
