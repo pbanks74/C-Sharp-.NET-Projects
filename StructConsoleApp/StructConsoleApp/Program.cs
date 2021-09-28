d76b83d92704660650660164aed210ef0ed16f8a
Paul Fairbanks <paulbanksinc16@gmail.com>
2021-09-28 13:51:54 -0700

    create app

---
+++ StructConsoleApp/StructConsoleApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // creates instance and assigns its property
+            Number num1;
+            num1.Amount = 100.5m;
+
+            //prints to console
+            Console.WriteLine(num1.Amount);
+
+            Console.ReadLine();
+        }
+        // creates a struct and assigns it a property
+        struct Number
+        {
+            public decimal Amount;
+        }
+    }
+}
