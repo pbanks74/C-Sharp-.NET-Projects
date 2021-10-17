c3dfc1749cc092a54209f04e646d56bc17fc05e7
Paul Fairbanks <paulbanksinc16@gmail.com>
2021-10-16 19:11:38 -0700

    create app
    example of chaining constructors together

---
+++ ConstructorApp/ConstructorApp/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorApp
+{
+    public class Employee
+    {
+        // constructer chaining or re-using the code  
+        // if no last name is entered "Granks Banks" the company name will be assigned :-)
+        public Employee(string firstName) : this (firstName, "Grand Banks")
+        {
+        }
+
+        public Employee (string firstName, string lastName)
+        {
+            string First = firstName;
+            string Last = lastName;
+        }
+    }
+}
---
+++ ConstructorApp/ConstructorApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // creates a constant field
+            const string buisnessName = "Grand Banks Hotel";
+
+            // creates an implicit type variable
+            var taxCode = 0017395;
+
+            // prints to the console
+            Console.WriteLine("Employees of " + buisnessName);
+            Console.WriteLine("Your Tax Code is: " + taxCode);
+
+            // creates a new employee using the method made of 2 constructors
+            Employee emp1 = new Employee("Paul");
+
+        
+            Console.ReadLine();
+        }
+    }
+}
