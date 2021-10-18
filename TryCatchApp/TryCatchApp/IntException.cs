e715546f3b9c3eba17938f6079e2f4b4ebfc5f28
Paul Fairbanks <paulbanksinc16@gmail.com>
2021-10-18 10:52:09 -0700

    create app

---
+++ TryCatchApp/TryCatchApp/IntException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatchApp
+{
+    public class IntException : Exception
+    {
+        public IntException()
+            : base() { }
+
+        public IntException(string message)
+            : base(message) { }
+    }
+
+}
---
+++ TryCatchApp/TryCatchApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatchApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // asks the user to input their age
+            Console.WriteLine("Please enter your age.");
+
+            // converts user input to int type and saves as a variable
+            int userAge = Convert.ToInt32(Console.ReadLine());
+
+            try
+            {
+                int yearBorn = 2021 - userAge;
+                Console.WriteLine("You were born in " + yearBorn);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Please enter only digits");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
